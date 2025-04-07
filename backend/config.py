c3ae54d7a5ba85e133cde0a63f47e321f0c72de9
chinzz2024 <pranavmohanindeevaram@gmail.com>
2025-04-08 02:07:49 +0530

    gj

--- backend/config.py
+++ backend/config.py
@@ -3,5 +3,5 @@ import os
 class Config:
     MC_EMAIL = os.getenv('MC_EMAIL', 'joleoran@gmail.com')
     MC_PASSWORD = os.getenv('MC_PASSWORD', 'iXQDIX@pZ4')
-    MC_LOGIN_URL = "https://www.moneycontrol.com/mc/login"
+    MC_LOGIN_URL = "https://m.moneycontrol.com/login.php?cpurl=https://www.moneycontrol.com/"
     MC_BASE_URL = "https://www.moneycontrol.com/news/business/stocks/"
